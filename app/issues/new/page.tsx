985c4e9757f80c28fb909d88e8e96f1442e03088
Ruby Chang <ruby931109@gmail.com>
2023-11-14 20:00:59 -0500

    disable ssr

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Button, TextArea, TextField, Callout, Text } from "@radix-ui/themes";
 import React, { useState } from "react";
-import SimpleMDE from "react-simplemde-editor";
+import dynamic from "next/dynamic";
 import { useForm, Controller } from "react-hook-form";
 import "easymde/dist/easymde.min.css";
 import axios from "axios";
@@ -11,6 +11,10 @@ import { createIssueSchema } from "@/app/validationSchemas";
 import { ZodLazy, z } from "zod";
 import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
