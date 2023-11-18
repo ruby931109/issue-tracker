3c720255213217abb3ee335bc272a4e5bd19dd7a
Ruby Chang <ruby931109@gmail.com>
2023-11-17 22:21:02 -0500

    improve the loading style

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,4 +1,11 @@
-import IssueForm from "../_components/IssueForm";
+import dynamic from "next/dynamic";
+import Skeleton from "react-loading-skeleton";
+import IssueFormSkeleton from "../_components/IssueFormSkeleton";
+
+const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
+  ssr: false,
+  loading: () => <IssueFormSkeleton />,
+});
 
 const NewIssuePage = () => {
   return <IssueForm />;
